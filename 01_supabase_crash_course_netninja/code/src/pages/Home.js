f238254d10e75874f370d0498952b3d3aea1692e
Mark Connelly <com0004@gmail.com>
2022-09-07 23:16:09 +1000

    Add code to repo

---
+++ 01_supabase_crash_course_netninja/code/src/pages/Home.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
+
+import SmoothieCard from "../components/SmoothieCard";
+
+const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+      if (error) {
+        setFetchError("Could not fetch the smoothies...");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+    fetchSmoothies();
+  }, []);
+
+  return (
+    <div className="page home">
+      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* Order-by buttons */}
+          {/* Smoothie Grid */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
